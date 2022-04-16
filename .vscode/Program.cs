95d82b383947ef8918f1a9bddc330a391cffa8f3
Allan-SE <allanalvesp@gmail.com>
2022-04-16 17:02:49 -0300

    First Commit

---
+++ .vscode/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace GetRestTest
+{
+    class Program
+    {
+        HttpClient client = new HttpClient();
+
+        static async Task Main(string[] args)
+        {
+            Program program = new Program();
+            await program.GetTodoItems();
+        }
+        private async Task GetTodoItems()
+        {
+            string response = await client.GetStringAsync(
+                "https://api.spacexdata.com/v3/launches");
+
+            Console.Write(response);
+        }
+        
+    }      
+}
